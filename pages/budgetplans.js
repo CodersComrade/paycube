3651e12e6b79c20fb587ae00b4ed2845268b57ed
RomanSheikh99 <romansheikh170@gmail.com>
2022-04-10 09:10:05 +0600

    push to my branch

--- pages/budgetplans.js
+++ pages/budgetplans.js
@@ -1,6 +1,33 @@
 import Sidenav from "../components/Sidenav";
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { Button, Modal } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import BudgetCard from "../components/BudgetCard";
 
 const Budgetplans = () => {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const [budget, setBudget] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/getBudget')
+            .then(res => res.json())
+            .then(data => setBudget(data))
+    },[])
+
+    const { register, handleSubmit, reset } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+        axios.post('http://localhost:5000/budget', data).then(res => {
+            if (res.data.insertedId) {
+                handleClose();
+                alert('added successfully');
+                reset();
+            }
+        });
+    };
     return (
         <>
             <div className="container-fluid dashboard">
@@ -9,7 +36,59 @@ const Budgetplans = () => {
                         <Sidenav></Sidenav>
                     </div>
                     <div className="col-md-10 dashboard bg-dark">
+                        <div className="row">
+                            {
+                                budget.map(data => <BudgetCard key={data._id} budget={data}></BudgetCard>)
+                            }
+                        </div>
 
+                        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                            <button className='btn btn-danger px-4 py-2 text-capitalize rounded-pill mx-auto my-3' onClick={handleShow}> Make New Budget</button>
+                        </div>
+                        <Modal show={show} onHide={handleClose}>
+                            <Modal.Header>
+                                <Modal.Title>Add New Budget</Modal.Title>
+                            </Modal.Header>
+                            <form
+                                className="p-4"
+                                onSubmit={handleSubmit(onSubmit)}
+                            >
+                                <input
+                                    style={{
+                                        border: '2px solid #32AEB1',
+                                        borderRadius: '5px',
+                                        padding: '10px',
+                                    }}
+                                    className="mt-2 me-1"
+                                    placeholder="budget name"
+                                    {...register('name', { required: true })}
+                                />
+                                <input
+                                    style={{
+                                        border: '2px solid #32AEB1',
+                                        borderRadius: '5px',
+                                        padding: '10px',
+                                    }}
+                                    className="mt-2"
+                                    placeholder="max budget"
+                                    {...register('budget', { required: true })}
+                                />
+                                <textarea
+                                    style={{
+                                        border: '2px solid #32AEB1',
+                                        borderRadius: '5px',
+                                        padding: '10px',
+                                    }}
+                                    className="mt-2 w-100"
+                                    placeholder="write why you create this budget"
+                                    {...register('description', { required: true })}
+                                />
+                               
+                                <input className="btn btn-info w-50 mx-auto " type="submit" value='create' />
+                            </form>
+                        </Modal>
+
+                        
                     </div>
                 </div>
             </div>
